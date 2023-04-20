b0a3e630fd002043b3202fd0b8763a7a42be534a
vip65 <130646010+vip65@users.noreply.github.com>
2023-04-20 11:58:45 +0800

    Update and rename iii/dd/dd.js to 罗马数字3/截止日期（Deadline Date的缩写）/dd.js

--- iii/dd/dd.js
+++
@@ -1,6 +0,0 @@
-﻿<script>
-  setTimeout(function(){
-   var arr=['http://baidu.com','http://qq.com','http://meitu.com'];
-  window.location.href=arr[parseInt(Math.random()*arr.length)];
-  },100);
-  </script>
