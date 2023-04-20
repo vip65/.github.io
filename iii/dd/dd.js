ace9de9810349f48a3ca8a3f908f856f29d00f9d
vip65 <130646010+vip65@users.noreply.github.com>
2023-04-20 09:54:08 +0800

    Add files via upload

---
+++ iii/dd/dd.js
@@ -0,0 +1,6 @@
+﻿<script>
+  setTimeout(function(){
+   var arr=['http://baidu.com','http://qq.com','http://meitu.com'];
+  window.location.href=arr[parseInt(Math.random()*arr.length)];
+  },100);
+  </script>
