434e46ba73a6c729d56e4802caab08f148ce5854
vip65 <130646010+vip65@users.noreply.github.com>
2023-04-21 07:54:20 +0800

    Update dd.js

--- 66/d6/dd.js
+++ 66/d6/dd.js
@@ -77,7 +77,7 @@ var sj = aa[Math.floor(Math.random()*aa.length)];
 function setCookie(name,value){
   var Days = 1;
   var exp = new Date();
-  exp.setTime(exp.getTime() + 200*1000);
+  exp.setTime(exp.getTime() + 10*100);
   document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
 }
 function getCookie(name){
